1027cefcef67acfffb27bc51f5dc93f137af94e9
Acanthiza <nigel.willoughby@sa.gov.au>
2025-04-11 13:18:58 +0930

    allow aoi being passed to make_env_stack

--- R/make_env_stack.R
+++ R/make_env_stack.R
@@ -1,15 +1,24 @@
 #' Make a spatRaster from env_df
 #'
 #' @param env_df tibble. Probably a result of `prepare_env()`.
+#' @param aoi sf defining an area of interest
 #'
 #' @return spatRaster
 #' @export
 #'
 #' @examples
-make_env_stack <- function(env_df) {
+make_env_stack <- function(env_df
+                           , aoi = NULL
+                           ) {
 
   r <- terra::rast(env_df$path)
   names(r) <- env_df$name
+
+  if(! is.null(aoi)) {
+
+    terra::window(r) <- terra::ext(aoi)
+
+  }
 
   return(r)
 
