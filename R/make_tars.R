9aa6342a6a57456d17ab4aa5525754e263130adf
Acanthiza <nigel.willoughby@sa.gov.au>
2025-05-20 16:35:13 +0930

    make_tars overhaul

--- R/make_tars.R
+++ R/make_tars.R
@@ -1,5 +1,5 @@
 
-#' Make targets
+#' Make tars
 #'
 #' Runs `targets::tar_source()`, creates and then reads _targets.yaml from any
 #' files matching the pattern ``.
@@ -7,7 +7,7 @@
 #' @param settings Character path to yaml settings file or named, nested list.
 #' Passed to `set_list` argument of `envFunc::name_env_out()`. If character, the
 #' named, nested list will be `yaml::read_yaml(settings)$context`
-#' @param base_dir Character. Path at which to create the store.
+#' @param store_base Character. Path at which to create the store.
 #' @param target_pattern Character. Regexp to find targets ['project files']
 #' (https://books.ropensci.org/targets/projects.html#project-files).
 #'
@@ -17,20 +17,25 @@
 #' @export
 #'
 #' @examples
-make_targets <- function(settings = "settings/setup.yaml"
-                         , base_dir = fs::path("..", "..", "out")
-                         , target_pattern = "^\\d{3}_.*\\.R$"
-                         ) {
+make_tars <- function(settings = "settings/setup.yaml"
+                      , store_base = fs::path("..", "..", "out")
+                      , script_base = "."
+                      , target_pattern = "^\\d{3}_.*\\.R$"
+                      , save_yaml = TRUE
+                      ) {
 
   set_list <- if(is.character(settings)) yaml::read_yaml(settings)$context else settings
 
   # tars --------
   ## tars df ------
-  tars_df <- tibble::tibble(script = fs::dir_ls(regexp = target_pattern)) |>
+  tars_df <- tibble::tibble(script = fs::dir_ls(script_base
+                                                , regexp = target_pattern
+                                                )
+                            ) |>
     dplyr::mutate(project = purrr::map_chr(script, \(x) gsub("\\d{3}_|\\.R", "", x))
                   , order = readr::parse_number(script)
                   , store = envTargets::store_dir(set_list = set_list
-                                                  , base_dir = base_dir
+                                                  , base_dir = store_base
                                                   )
                   , store = fs::path(store, project)
                   ) |>
@@ -38,14 +43,27 @@ make_targets <- function(settings = "settings/setup.yaml"
     dplyr::select(project, script, store)
 
   ## _targets.yaml -------
-  if(file.exists("_targets.yaml")) fs::file_delete("_targets.yaml")
+  if(! save_yaml) {
+
+    out_yml <- tempfile(fileext = ".yaml")
+
+    Sys.setenv(TAR_CONFIG = tmp_yml)
+
+  } else {
+
+    out_yml <- "_targets.yaml"
+
+  }
+
+  if(file.exists(out_yml)) fs::file_delete(out_yml)
 
   purrr::pmap(tars_df
               , targets::tar_config_set
+              , config = out_yml
               )
 
   ## tars list ------
-  tars <- yaml::read_yaml("_targets.yaml")
+  tars <- yaml::read_yaml(out_yml)
 
   return(tars)
 
