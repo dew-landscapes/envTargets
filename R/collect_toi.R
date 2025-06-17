6213e34d766a4bf635fab04f00ae9e53d0175c01
Acanthiza <nigel.willoughby@sa.gov.au>
2025-06-17 10:59:31 +0930

    simplify collect_toi

--- R/collect_toi.R
+++ R/collect_toi.R
@@ -5,10 +5,11 @@
 #' @param dir Directory in which `toi_regex` files will be found
 #' @param toi_regex Regular expression that will pick up any yaml files
 #' containing the taxa of interest.
-#' @param environ_df Dataframe of environments (e.g. terrestrial, marine).
-#' @param environ_remove Character. Vector of environments to remove.
-#' @param other_taxa Character. Vector of taxa used to limit the resulting
-#' vector. i.e. only taxa that appear in `other_taxa` will make it into the
+#' @param exclude Character. Vector of taxa used to limit the resulting
+#' vector. i.e. only taxa that do not appear in `exlude` will make it into
+#' the resulting vector.
+#' @param include Character. Vector of taxa used to limit the resulting
+#' vector. i.e. only taxa that appear in `include` will make it into the
 #' resulting vector.
 #'
 #' @return Character vector
@@ -17,38 +18,40 @@
 #' @examples
 collect_toi <- function(dir = "settings"
                         , toi_regex = "\\/toi"
-                        , environ_df = NULL
-                        , environ_remove = "marine"
-                        , other_taxa = NULL
+                        , exclude = NULL
+                        , include = NULL
+                        , keep_previous = TRUE
+                        , store_path
                         ) {
 
-  keep_taxa <- fs::dir_ls(dir, regexp = toi_regex) |>
-    purrr::map(yaml::read_yaml) |>
-    unlist() |>
-    unname() |>
+  previous <- if(keep_previous) {
+
+    tar_read(toi, store = store_path)
+
+  } else NULL
+
+  keep <- c(previous
+            , fs::dir_ls(dir, regexp = toi_regex) |>
+              purrr::map(yaml::read_yaml) |>
+              unlist() |>
+              unname()
+            ) |>
     unique() |>
     sort()
 
-  if(!is.null(other_taxa)) {
+  if(!is.null(include)) {
 
-    keep_taxa <- other_taxa |>
-      intersect(keep_taxa)
-
-  }
-
-  if(!is.null(environ_df)) {
-
-    keep_taxa <- environ_df |>
-      dplyr::filter(! grepl(paste0(environ_remove, collapse = "|")
-                            , environ
-                            )
-                    ) |>
-      dplyr::distinct(taxa) |>
-      dplyr::pull(taxa) |>
-      intersect(keep_taxa)
+    keep <- include |>
+      intersect(keep)
 
   }
 
-  return(keep_taxa)
+  if(!is.null(exclude)) {
+
+    keep <- setdiff(keep, exclude)
+
+  }
+
+  return(keep)
 
 }
