d2220f16086e58adb80c88db60ff3f4a16157792
Acanthiza <nigel.willoughby@sa.gov.au>
2025-09-10 07:26:06 +0930

    render with deps changes

--- R/render_with_deps.R
+++ R/render_with_deps.R
@@ -1,6 +1,9 @@
 
 #' Render bookdown and force Rmd file dependencies
 #'
+#' Moving the report to the targets store relies on output_dir being specified
+#' in the _bookdown.yaml file, usually written within report_prep.R
+#'
 #' Enables a bookdown 'target' within a [targets workflow](https://books.ropensci.org/targets/).
 #' I was unable to get [bookdowntargets](https://mps9506.r-universe.dev/bookdowntargets/doc/manual.html)
 #' to work without error. That may be a better way to achieve this. Currently
@@ -10,27 +13,44 @@
 #'
 #' Based on code found here: https://github.com/shirdekel/phd_thesis/commit/cc66e1b9d9305a4b21e8489836545ecc4475f9ee
 #'
-#' @param input_directory Path in which `index.Rmd` can be found
-#' @param output_directory Directory path. Where should the final results be
-#' saved (this is usually not a targets store).
-#' @param clean_up Logical. Delete intermediary _and_ `output_directory` _before_
-#' knit and delete intermediary files _after_ knit.
-#' @return The target is just the path `output_directory`. Files needed for the
-#' book are written into `output_directory`.
+#' @param input_directory Path in which `index.Rmd` can be found.
+#' @param deps Any dependencies for the report. Note that these are not made
+#' available within the knit, they just trigger the target to run.
+#' @param remove_main Logical. Delete _main.Rmd froom the input directory before
+#' knit?
+#' @param clean_out_dir Logical. Delete the book before knit? Useful if chapter
+#' names are changed, leaving orphaned `.html` files in the output directory.
+#' @param default_out_dir If _bookdown.yaml does not specify the `output_dir`,
+#' use this output directory instead.
+#' @param clean_up Logical. Delete intermediary files after knit?
+#'
+#' @return Return value is just the `output_dir` specified in `_bookdown.yaml`,
+#' or, if that is not specified, the value of `default_out_dir`. Files needed
+#' for the book are written into the output directory.
+#'
 #' @author Shir Dekel (modified by nw)
 #' @export
 render_with_deps <- function(input_directory = "."
                              , deps
-                             , output_directory = NULL
+                             , remove_main = TRUE
+                             , clean_out_dir = TRUE
+                             , default_out_dir = "report"
                              , clean_up = TRUE
                              ) {
 
-  if(clean_up) {
+  output_dir <- yaml::read_yaml("report/_bookdown.yaml")$output_dir
 
-    unlink(c(fs::path(input_directory, "_main.Rmd")
-             , output_directory
-             )
-           )
+  if(is.null(output_dir)) output_dir <- default_out_dir
+
+  if(remove_main) {
+
+    unlink(fs::path(input_directory, "_main.Rmd"))
+
+  }
+
+  if(clean_out_dir) {
+
+    unlink(output_dir)
 
   }
 
@@ -68,6 +88,6 @@ render_with_deps <- function(input_directory = "."
 
   }
 
-  return(output_directory)
+  return(output_dir)
 
 }
