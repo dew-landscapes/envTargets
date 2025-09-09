102c54cb56b497ff49363219ffa4ba7da9104f5a
Acanthiza <nigel.willoughby@sa.gov.au>
2025-09-10 08:06:47 +0930

    prepare_bookdown_yaml

---
+++ R/prepare_bookdown_yaml.R
@@ -0,0 +1,48 @@
+#' Prepare _bookdown.yaml within a targets workflow
+#'
+#' @param report_dir Character. Where should the `_bookdown.yaml` file be saved?
+#' @param store Character. Path to the targets store into which the report
+#' should be saved
+#' @param output_dir Character. Name to give the folder in `store` where the
+#' report will be saved.
+#'
+#' @returns Path to saved `_bookdown.yaml`.
+#' @export
+#'
+#' @examples
+prepare_bookdown_yaml <- function(report_dir = "report"
+                                  , store = tars$report$store
+                                  , output_dir = "compiled_report"
+                                  ) {
+
+  out_file <- fs::path(report_dir, "_bookdown.yaml")
+
+  proj <- basename(here::here())
+
+  ymlthis::yml_empty() |>
+    ymlthis::yml_bookdown_opts(delete_merged_file = TRUE
+                               , edit = paste0("https =//github.com/dew-landscapes/"
+                                               , proj
+                                               , "/edit/master/%s"
+                                               )
+                               , rmd_files = c("index.Rmd"
+                                               , fs::dir_ls(path = "report"
+                                                            , regexp = "\\d{4}.*Rmd$"
+                                                            ) |>
+                                                 gsub("report/", "", x = _) |>
+                                                 unname()
+                                               )
+                               , repo = gsub("\\.git$", "", usethis::git_remotes()$origin)
+                               , output_dir = fs::path(store |>
+                                                         gsub("\\.\\.\\/\\.\\."
+                                                              , "../../.."
+                                                              , x = _
+                                                              )
+                                                       , output_dir
+                                                       )
+                               ) |>
+    yaml::write_yaml(out_file)
+
+  return(out_file)
+
+}
