2c3d79d4117ae6249bac0ef821ae5eff9441314a
Acanthiza <nigel.willoughby@sa.gov.au>
2026-02-16 21:49:32 +1030

    Write scales.yaml during write_tars

--- R/write_tars.R
+++ R/write_tars.R
@@ -2,7 +2,8 @@
 #'
 #' Writes a `tars` object to `here::here("_targets.yaml")` (i.e. the script
 #' directory) and `fs::path(dirname(tars[[1]]$store), "_targets.yaml")` (i.e.
-#' the store directory). Optionally exports a .csv of package versions based on
+#' the store directory). Write `scales.yaml` into the store directory.
+#' Optionally exports a .csv of package versions based on
 #' packages listed in `package_yaml`.
 #'
 #' @param tars Tars list
@@ -10,15 +11,18 @@
 #' `sessioninfo::package_info()`) to the store (as .csv).
 #' @param package_yaml Path to yaml file containing packages to be loaded by
 #' scripts.
+#' @param scales_yaml Path to yaml file containing 'scales' (e.g. extent, grain
+#' and possibly area-of-interest (aoi)).
 #'
-#' @return `NULL`. Two `_targets.yaml` files written. Optionally,
-#' `package_info.csv` written.
+#' @return `NULL`. Two `_targets.yaml` and one `scales_yaml` files written.
+#' Optionally, `package_info.csv` written.
 #' @export
 #'
 #' @examples
 write_tars <- function(tars
                        , package_dump = TRUE
                        , package_yaml = "settings/packages.yaml"
+                       , scales_yaml = "settings/scales.yaml"
                        ) {
 
   yaml::write_yaml(tars
@@ -32,6 +36,18 @@ write_tars <- function(tars
   yaml::write_yaml(tars
                    , fs::path(store_dir, "_targets.yaml")
                    )
+
+  if(file.exists(scales_yaml)) {
+
+    fs::copy(scales_yaml
+             , fs::path(store_dir, "scales.yaml")
+             )
+
+  } else {
+
+    warning("scales_yaml file not found, so scales yamls not copied to store directory")
+
+  }
 
   if(package_dump) {
 
