7212c82dab3f37faeb5d273862374f32260646ee
acanthiza <nigel.willoughby@sa.gov.au>
2026-05-27 11:21:20 +0930

    env_tar_group to help with dew-landscapes/envCleaned#26

---
+++ R/env_tar_group.R
@@ -0,0 +1,44 @@
+#' Prepare a target for batched branching
+#'
+#' Helps to minimise long tails while processing batched data frames by taking
+#' into account one of the columns (`col`) within the dataframe (usually `n`)
+#' that is likely to be correlated with processing time. Remember to set
+#' `iteration = "group"` within the target.
+#'
+#' @param df dataframe
+#' @param col column to use when assigning rows to groups
+#' @param batches How many groups to create? Defaults to `use_cores`
+#'
+#' @returns `targets::tar_group()`ed dataframe
+#' @export
+#'
+#' @examples
+env_tar_group <- function(df
+                          , col = "n"
+                          , batches = use_cores
+                          ) {
+
+  arr <- df |>
+    dplyr::arrange(desc(n))
+
+  n_rows <- nrow(arr)
+
+  res <- if(n_rows > cores) {
+
+    arr |>
+      dplyr::mutate(group = rep(1:use_cores, length.out = n_rows))
+
+  } else {
+
+    arr |>
+      dplyr::mutate(group = dplyr::row_number())
+
+  }
+
+  res <- res |>
+    dplyr::group_by(group) |>
+    targets::tar_group()
+
+  return(res)
+
+}
