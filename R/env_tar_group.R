150c28d48fd228db5ec5f4edca8cf02424924081
acanthiza <nigel.willoughby@sa.gov.au>
2026-06-11 09:29:18 +0930

    debug env_tar_group

--- R/env_tar_group.R
+++ R/env_tar_group.R
@@ -1,12 +1,12 @@
 #' Prepare a target for batched branching
 #'
 #' Helps to minimise long tails while processing batched data frames by taking
-#' into account one of the columns (`col`) within the dataframe (usually `n`)
+#' into account one of the columns (`column`) within the dataframe (usually `n`)
 #' that is likely to be correlated with processing time. Remember to set
 #' `iteration = "group"` within the target.
 #'
 #' @param df dataframe
-#' @param col column to use when assigning rows to groups
+#' @param column column to use when assigning rows to groups
 #' @param batches How many groups to create?
 #'
 #' @returns `targets::tar_group()`ed dataframe
@@ -14,12 +14,12 @@
 #'
 #' @examples
 env_tar_group <- function(df
-                          , col = "n"
+                          , column = "n"
                           , batches = envFunc::use_cores(absolute_max = yaml::read_yaml("settings/setup.yaml")$max_cores)
                           ) {
 
   arr <- df |>
-    dplyr::arrange(desc(n))
+    dplyr::arrange(desc(!!rlang::ensym(column)))
 
   n_rows <- nrow(arr)
 
